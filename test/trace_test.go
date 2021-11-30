76b23ef690ed1d0f2a5e7a8332f19714f4668707
liuqinghui <liuqinghui@weimiao.cn>
2021-11-30 23:48:47 +0800

    add trace test

--- test/trace_test.go
+++ test/trace_test.go
@@ -1,32 +1,67 @@
 package test
 
 import (
+	"context"
+	"fmt"
+	"os"
+	"runtime/trace"
+	"sync"
 	"testing"
 	"time"
 )
 
 func TestUseTrace(t *testing.T) {
+	//runtime.GOMAXPROCS()
+	selectDB()
+	writeFile()
+	data := map[int]time.Time{}
 	for i := 0; i < 100; i++ {
-		a := i
-		selectDB()
-		writeFile()
-		sum(a)
+		data[i] = time.Now()
 	}
+	fmt.Println("len=",len(data))
 }
 
 func selectDB() {
-	time.Sleep(time.Millisecond * 50)
-	_ = make(map[string]string, 10000)
+	time.Sleep(time.Second * 1)
 }
 
 func writeFile() {
-	time.Sleep(time.Millisecond * 100)
-	_ = make(map[string]string, 3000)
+	time.Sleep(time.Second * 2)
 }
 
-var total int
+func TestCreateCou(t *testing.T) {
+	file, err := os.Create("./mytask.out")
+	if err != nil {
+		fmt.Printf("%v\n", err)
+		return
+	}
+	defer file.Close()
+	err = trace.Start(file)
+	if err != nil {
+		fmt.Printf("%v\n", err)
+		return
+	}
+	defer trace.Stop()
 
-func sum(i int) {
-	time.Sleep(time.Millisecond * 40)
-	total = total + i
+	// 创建自定义任务
+	ctx, task := trace.NewTask(context.Background(), "myTask")
+	defer task.End()
+	var wg sync.WaitGroup
+	for i := 0; i < 10; i++ {
+		gn := i
+		// 启动协程
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			trace.WithRegion(ctx, fmt.Sprintf("goroutine-%d",gn), func() {
+				sum := 0
+				for n := 0; n < 1000000; n++ {
+					sum = sum + n
+				}
+				fmt.Println("sum = ",sum)
+			})
+		}()
+	}
+	wg.Wait()
+	fmt.Println("run ok!")
 }
